66a12f1512397b892c5dbc5d5a7e13cf0dcba5ef
謝明華 Minghua Hsieh <minghua@alum.ccu.edu.tw>
2023-11-04 16:02:18 +0800

    Update controllerResponse.go

--- web/controller/controllerResponse.go
+++ web/controller/controllerResponse.go
@@ -11,19 +11,19 @@ import (
 
 func ShowView(w http.ResponseWriter, r *http.Request, templateName string, data interface{})  {
 
-	// 指定视图所在路径
+	// 指定視圖所在路徑
 	pagePath := filepath.Join("web", "tpl", templateName)
 
 	resultTemplate, err := template.ParseFiles(pagePath)
 	if err != nil {
-		fmt.Printf("创建模板实例错误: %v", err)
+		fmt.Printf("創建模板實例錯誤: %v", err)
 		return
 	}
 
 	err = resultTemplate.Execute(w, data)
 	if err != nil {
-		fmt.Printf("在模板中融合数据时发生错误: %v", err)
-		//fmt.Fprintf(w, "显示在客户端浏览器中的错误信息")
+		fmt.Printf("在模板中融合數據時發生錯誤: %v", err)
+		//fmt.Fprintf(w, "顯示在客戶端瀏覽器中的錯誤信息")
 		return
 	}
 
